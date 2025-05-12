776418f14a78a3a7b8d3d5ff0317e6b22afc8293
lai2-24 <lai2-24@itismeucci.com>
2025-05-12 13:46:53 +0200

    pary?

--- app/src/App.js
+++ app/src/App.js
@@ -1,36 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './TicTacToe.css';
 
 export default function App() {
   const [showTable, setShowTable] = useState(false);
+  const [alunni, setAlunni] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/alunni/1/1') // Adjust the URL as needed
+      .then(response => response.json())
+      .then(data => setAlunni(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   const toggleTable = () => {
     setShowTable(!showTable);
   };
 
-  const alunni = [
-    {
-      "nome": "Ivan",
-      "cognome": "Bruno",
-      "classe_id": "1"
-    },
-    {
-      "nome": "Claudio",
-      "cognome": "Benve",
-      "classe_id": "1"
-    },
-    {
-        "nome": "Matteo",
-        "cognome": "Ciardi",
-        "classe_id": "2"
-      }
-  ];
-
   return (
     <div className="App">
       <button onClick={toggleTable}>Show/Hide Alunni Table</button>
       {showTable && (
-        <table>
+        <table className="table">
           <thead>
             <tr>
               <th>Nome</th>
@@ -52,3 +42,4 @@ export default function App() {
     </div>
   );
 }
+
